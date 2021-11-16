ff2b0ebe0c58397e93ec66d4c62d673a6fc78d76
UygarKAYA <uygar.kaya@ozu.edu.tr>
2021-11-17 02:15:36 +0300

    DataFrame was created for each Technical Indicator

--- script.py
+++ script.py
@@ -1,29 +1,46 @@
 import yfinance as yf
-from talib import *
-from matplotlib import pyplot as plt
+import pandas as pd
 import numpy as np
 
-startDate = '2011-11-07'
-endDate = '2021-11-07'
+from matplotlib import pyplot as plt
+from talib import RSI, WMA, EMA, SMA
+from talib import ROC, CMO, CCI, PPO
+from talib import TEMA, WILLR, MACD
+
+startDate = '2011-11-09'
+endDate = '2021-11-11'
 coins = ['SPY']
 
-spy_data = yf.download('SPY', start=startDate, end=endDate)
+spy_data = yf.download('SPY', start = startDate, end = endDate)
 
-rsi = RSI(spy_data["Close"], timeperiod=14)
-willr = WILLR(spy_data["High"], spy_data["Low"], spy_data["Close"], timeperiod=14)
-wma = WMA(spy_data["Close"], timeperiod=30)
-ema = EMA(spy_data["Close"], timeperiod=30)
-sma = SMA(spy_data["Close"], timeperiod=30)
-# hma yok !?
-tema = TEMA(spy_data["Close"], timeperiod=30)
-cci = CCI(spy_data["High"], spy_data["Low"], spy_data["Close"], timeperiod=14)
-cmo = CMO(spy_data["Close"], timeperiod=14)
+# DataFrame Preparation
+rsi = RSI(spy_data["Close"], timeperiod=14).to_frame().reset_index().set_axis(['Date', 'Value'], axis=1)
+wma = WMA(spy_data["Close"], timeperiod=30).to_frame().reset_index().set_axis(['Date', 'Value'], axis=1)
+ema = EMA(spy_data["Close"], timeperiod=30).to_frame().reset_index().set_axis(['Date', 'Value'], axis=1)
+sma = SMA(spy_data["Close"], timeperiod=30).to_frame().reset_index().set_axis(['Date', 'Value'], axis=1)
+roc = ROC(spy_data["Close"], timeperiod=10).to_frame().reset_index().set_axis(['Date', 'Value'], axis=1)
+cmo = CMO(spy_data["Close"], timeperiod=14).to_frame().reset_index().set_axis(['Date', 'Value'], axis=1)
+cci = CCI(spy_data["High"], spy_data["Low"], spy_data["Close"], timeperiod=14).to_frame().reset_index().set_axis(['Date', 'Value'], axis=1)
+ppo = PPO(spy_data["Close"], fastperiod=12, slowperiod=26, matype=0).to_frame().reset_index().set_axis(['Date', 'Value'], axis=1)
+tema  = TEMA(spy_data["Close"], timeperiod=30).to_frame().reset_index().set_axis(['Date', 'Value'], axis=1)
+willr = WILLR(spy_data["High"], spy_data["Low"], spy_data["Close"], timeperiod=14).to_frame().reset_index().set_axis(['Date', 'Value'], axis=1)
 macd, macdsignal, macdhist = MACD(spy_data["Close"], fastperiod=12, slowperiod=26, signalperiod=9)
-ppo = PPO(spy_data["Close"], fastperiod=12, slowperiod=26, matype=0)
-roc = ROC(spy_data["Close"], timeperiod=10)
-# cmfi yok !?
-# dmi yok !?
-# psi yok !?
 
-rsi = rsi.reset_index()
-print(rsi)
+macd = macd.to_frame().reset_index().set_axis(['Date', 'Value'], axis=1)
+macdsignal = macdsignal.to_frame().reset_index().set_axis(['Date', 'Value'], axis=1)
+macdhist = macdhist.to_frame().reset_index().set_axis(['Date', 'Value'], axis=1)
+
+# Dealing with missing values and replacing them by their mean
+rsi['Value'].fillna(rsi['Value'].mean(), inplace=True)
+wma['Value'].fillna(wma['Value'].mean(), inplace=True)
+ema['Value'].fillna(ema['Value'].mean(), inplace=True)
+sma['Value'].fillna(sma['Value'].mean(), inplace=True)
+roc['Value'].fillna(roc['Value'].mean(), inplace=True)
+cmo['Value'].fillna(cmo['Value'].mean(), inplace=True)
+cci['Value'].fillna(cci['Value'].mean(), inplace=True)
+ppo['Value'].fillna(ppo['Value'].mean(), inplace=True)
+tema['Value'].fillna(tema['Value'].mean(), inplace=True)
+willr['Value'].fillna(willr['Value'].mean(), inplace=True)
+macd['Value'].fillna(macd['Value'].mean(), inplace=True)
+macdsignal['Value'].fillna(macdsignal['Value'].mean(), inplace=True)
+macdhist['Value'].fillna(macdhist['Value'].mean(), inplace=True)
